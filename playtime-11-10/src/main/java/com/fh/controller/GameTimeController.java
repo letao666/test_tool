949fdc6bc8ec986a2072628e198d2dcc1798adb6
letao666 <74390254+letao666@users.noreply.github.com>
2020-11-14 14:22:26 +0800

    Update GameTimeController.java

--- playtime-11-10/src/main/java/com/fh/controller/GameTimeController.java
+++ playtime-11-10/src/main/java/com/fh/controller/GameTimeController.java
@@ -19,7 +19,7 @@ import javax.servlet.http.HttpServletRequest;
 public class GameTimeController {
 
     public static void main(String[] args) {
-        System.out.println("1111");
+        System.out.println("2222");
     }
 
 
